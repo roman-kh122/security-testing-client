2a319e0fb2d583f27cee66a1a5a2a7a9171d6dde
Roman Kharchyshyn <roman.kharchyshyn.pz.2021@lpnu.ua>
2024-11-25 19:02:22 +0200

    new url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5134/api",
+  baseURL: "https://securitytestingapimain.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
